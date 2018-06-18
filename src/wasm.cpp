48ddf019bd0172cd6c5b4eec9267713588c39131
Robert Hitchcock <roberthitchcock163@gmail.com>
2018-06-18 06:12:32 -0400

    Restructure

--- src/wasm.cpp
+++ src/wasm.cpp
@@ -1,7 +1,15 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <cstring>
+#include <cstdint>
+
 #include "wasm.hpp"
 
+const std::uint32_t magic_number = 0x6D736100;
+
 namespace wasm {
-  module::module (const std::string &file_path) {
+  module::module(const std::string &file_path) {
     std::ifstream file(file_path, std::ios::binary | std::ios::ate);
     if (!file.is_open()) {
       return;
@@ -11,7 +19,8 @@ namespace wasm {
     file.seekg(0, std::ios::beg);
     file.read(buffer, size);
     file.close();
-    if (*(std::uint32_t *)buffer != 0x6D736100) {
+    // Ensure file begins with magic number '\0asm'.
+    if (*reinterpret_cast<std::uint32_t *>(buffer) != magic_number) {
       return;
     }
     this->version = *(std::uint32_t *)(buffer + 4);
--- src/wasm.hpp
+++ src/wasm.hpp
@@ -1,20 +1,18 @@
 #ifndef WASM_HPP
 #define WASM_HPP
 
-#include <iostream>
-#include <fstream>
 #include <string>
-#include <cstring>
 #include <cstdint>
 
 namespace wasm {
   class module {
-    bool loaded = false;
   public:
     module(const std::string &file_path);
     ~module();
     bool is_loaded();
     std::uint32_t version;
+  private:
+    bool loaded = false;
   };
 }
 
